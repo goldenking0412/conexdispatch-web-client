39057df3c9f146a1932b4d00323339e219c0139e
Zowe Siouff <zowesiouff@gmail.com>
2017-05-31 11:05:30 +0200

    Settings Modal: Source Row: Order connected accounts by alphabetical orders on their ids

--- src/client/ui/settings_modal/sources_tab.jsx
+++ src/client/ui/settings_modal/sources_tab.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 /*!
  * kin
  * Copyright(c) 2016-2017 Benoit Person
@@ -6,13 +6,44 @@ import PropTypes from 'prop-types';
  */
 
 import React from "react";
-import _ from "lodash";
+import { flow, isNil } from "lodash";
+import { filter, join, map, orderBy } from "lodash/fp";
 
 import { source_prop_type } from "../../prop_types";
 import { split_source_id, providers } from "../../utils";
 import SettingsModalSourceRow from "./source_row";
 
-const SettingsModalSourcesTab = props => {
+const mapUncaped = map.convert({ cap: false });
+
+const _get_sub_title = flow(
+    source => [source.display_name, source.email],
+    filter(val => !isNil(val)),
+    join(" - ")
+);
+
+const _render_connected_sources_row = (dispatch, sources) =>
+    flow(
+        orderBy("id", "asc"),
+        map(source => {
+            const { provider_name } = split_source_id(source.id);
+            return (
+                <SettingsModalSourceRow
+                  key={source.id}
+                  id={source.id}
+                  status={source.status}
+                  name={provider_name}
+                  sub_title={_get_sub_title(source)}
+                  dispatch={dispatch}
+                />
+            );
+        })
+    )(sources);
+
+const _render_available_sources_row = mapUncaped((provider, name) => (
+    <SettingsModalSourceRow key={name} name={name} sub_title={provider.settings_sub_title} />
+));
+
+const SettingsModalSourcesTab = ({ dispatch, sources }) => {
     return (
         <div className="tabs-panel" id="settings-accounts">
             <header>
@@ -20,15 +51,7 @@ const SettingsModalSourcesTab = props => {
             </header>
             <table className="hover available-sources">
                 <tbody>
-                    {_.map(providers, (provider, name) => {
-                        return (
-                            <SettingsModalSourceRow
-                              key={name}
-                              name={name}
-                              sub_title={provider.settings_sub_title}
-                            />
-                        );
-                    })}
+                    {_render_available_sources_row(providers)}
                 </tbody>
             </table>
 
@@ -37,22 +60,7 @@ const SettingsModalSourcesTab = props => {
             </header>
             <table className="hover connected-sources">
                 <tbody>
-                    {_.map(props.sources, source => {
-                        const { provider_name } = split_source_id(source.id);
-                        const sub_title = _([source.display_name, source.email])
-                            .filter(val => !_.isNil(val))
-                            .join(" - ");
-                        return (
-                            <SettingsModalSourceRow
-                              key={source.id}
-                              id={source.id}
-                              status={source.status}
-                              name={provider_name}
-                              sub_title={sub_title}
-                              dispatch={props.dispatch}
-                            />
-                        );
-                    })}
+                    {_render_connected_sources_row(dispatch, sources)}
                 </tbody>
             </table>
         </div>
